d617654272c1e5c05486a98c1c87293e2982e706
Aleksandar Avramovic <aleksandar.avramovic@forwardslashny.com>
2024-01-23 16:02:37 +0100

    Refactoring header and hero section to be responsive; Scrolling section added

---
+++ src/js/_scrollingSection.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $(".carousel").slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 9999,
+    arrows: false,
+    centerMode: true,
+    pauseOnHover: true,
+    ease: "linear",
+    cssEase: "linear",
+  });
+});
