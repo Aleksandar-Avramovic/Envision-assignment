0edf4ef37a35c3baa199741aa51a04a7b5a3c8d3
Aleksandar Avramovic <aleksandar.avramovic@forwardslashny.com>
2024-02-05 15:06:22 +0100

    slider section added

--- src/js/_slider.js
+++ src/js/_slider.js
@@ -1,14 +1,12 @@
 $(document).ready(function () {
-  $(".slider__card--container").slick({
+  $(".slider__container").slick({
     centerMode: true,
-    dots: true,
-    centerPadding: "60px",
-    slidesToShow: 1,
+    centerPadding: "0",
+    initialSlide: 2,
+    infinite: true,
+    slidesToShow: 3,
     slidesToScroll: 1,
     arrows: false,
-    customPaging: function (slider, i) {
-      return '<div class="custom-dot"><div class="inner-circle"></div></div>';
-    },
-    dotsClass: "custom-dots-container",
+    dots: true,
   });
 });
